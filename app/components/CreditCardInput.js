f878c6b3042abf593b87866aa9bd4ddf1b4c4444
Fernando <krajka.bartosz@gmail.com>
2015-08-18 18:49:49 +0200

    Component now sets new state on every input change. However both inputs' values are still immutable.

--- app/components/CreditCardInput.js
+++ app/components/CreditCardInput.js
@@ -4,17 +4,28 @@ class CreditCardInput extends React.Component {
   constructor(props) {
     super(props);
     this.state = { number: '' };
+    this.handleChange = this.handleChange.bind(this);
   }
 
   process(number) {
     return { text: '', type: '' };
+  }
+  
+  handleChange(e) {
+    let newValue = e.target.value;
+    let newNumber = this.filterWhitespace(newValue);
+    this.setState({ number: newNumber });
+  }
+
+  filterWhitespace(text) {
+    return text.replace(/\s/g, '');
   }
 
   render () {
     let { text: text, type: type } = this.process(this.state.number);
     return (
       <div>
-        <input type="text" value={text} />
+        <input type="text" value={text} onChange={this.handleChange} />
         <input type="text" value={type} readOnly />
       </div>
     );
