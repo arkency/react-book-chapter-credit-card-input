9ae3b950afe00dcd23baccd4fcd7d6f2b8f41551
Fernando <krajka.bartosz@gmail.com>
2015-08-18 18:48:10 +0200

    Types property added.

--- app/index.js
+++ app/index.js
@@ -4,4 +4,10 @@ import CreditCardInput from './components/CreditCardInput';
 const APP_ROOT = document.createElement('div');
 document.body.appendChild(APP_ROOT);
 
-React.render(<CreditCardInput />, APP_ROOT);
+let types = {
+  'Visa': /^4/,
+  'MasterCard': /^5[1-5]/,
+  'American Express': /^3[47]/
+};
+
+React.render(<CreditCardInput types={types}/>, APP_ROOT);
