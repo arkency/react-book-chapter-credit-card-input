48da7e53853a6ede61669fe5c8c2a015c95cce74
Fernando <krajka.bartosz@gmail.com>
2015-08-18 18:44:35 +0200

    Static component with immutable state. `process` function will return inputs' value all the time.

--- app/components/CreditCardInput.js
+++ app/components/CreditCardInput.js
@@ -1,8 +1,23 @@
 import React from 'react';
 
 class CreditCardInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { number: '' };
+  }
+
+  process(number) {
+    return { text: '', type: '' };
+  }
+
   render () {
-    return null;
+    let { text: text, type: type } = this.process(this.state.number);
+    return (
+      <div>
+        <input type="text" value={text} />
+        <input type="text" value={type} readOnly />
+      </div>
+    );
   }
 }
 
